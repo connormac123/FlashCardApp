905347b34740d9ef19ce584be688430ff44c7ee5
connormac123 <64983789+connormac123@users.noreply.github.com>
2024-05-25 14:56:39 -0600

    Rest of the files

--- app/src/main/java/com/example/flashcardapp/MainActivity.kt
+++ app/src/main/java/com/example/flashcardapp/MainActivity.kt
@@ -17,6 +17,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.flashcardapp.ui.theme.FlashCardAppTheme
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.material3.Card
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.unit.dp
 
 data class Deck(val name: String)
 
